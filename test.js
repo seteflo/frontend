d61c43d73fa9e0bdfbf9f6301073b04a63db0e17
unknown <Alumnos@SB309-17.upc.edu.pe>
2024-05-17 16:51:57 -0500

    se actualizo el codigo con compuestos, constantes, funciones y null

--- test.js
+++ test.js
@@ -1,3 +1,20 @@
 var EstaMal = true;
 console.log(EstaMal);
 console.log("hola");
+var a = []; // se usa Array <[tipo de dato]> = [] para crear una tupla
+a.push(2);
+a.push(5);
+a.push(7);
+a.push(9);
+console.log(a);
+//Compuestos -> Se le denomina información exacta 
+var docenteCompuesto = { nombre: "Juan", apellido: "Torres", edad: 25 };
+console.log(docenteCompuesto);
+//Constante
+var usuario = "u20191a458";
+console.log(usuario);
+//Solo se usará null como valor nulo
+//Funciones
+function promedio(a, b) {
+    return a + b;
+}
--- test.ts
+++ test.ts
@@ -2,3 +2,32 @@ let EstaMal: boolean = true
 
 console.log(EstaMal)
 console.log("hola")
+
+let a:Array<number> = []// se usa Array <[tipo de dato]> = [] para crear una tupla
+
+a.push(2)
+a.push(5)
+a.push(7)
+a.push(9)
+
+console.log(a);
+
+//Compuestos -> Se le denomina información exacta 
+
+let docenteCompuesto = {nombre: "Juan", apellido:"Torres", edad: 25}
+console.log(docenteCompuesto)
+
+
+//Constante
+
+const usuario: string = "u20191a458"
+
+console.log(usuario);
+
+//Solo se usará null como valor nulo
+
+//Funciones
+function promedio(a:number, b:number):number{
+    
+    return a+b
+}
